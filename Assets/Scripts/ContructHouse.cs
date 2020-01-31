689dcb5e84f1281b5ab7349ef4b3aa82f346b717
unknown <pipeyodavid@hotmail.com>
2020-01-31 23:13:45 +0100

    House build, damage to enemy

---
+++ Assets/Scripts/ContructHouse.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContructHouse : MonoBehaviour
+{
+
+    public int timeToConstruct;
+    private float startTime;
+    private bool isOnTrigger;
+
+    void Start()
+    {
+        isOnTrigger = false;
+    }
+
+    void Update()
+    {
+         if (Input.GetButtonDown("hammer"))
+         {
+            startTime = Time.time;
+         }
+        if (Input.GetButton("Fire1"))
+        {
+            if (isOnTrigger && Time.time - startTime >= timeToConstruct)
+            {
+                Debug.Log("Constructed");
+            }
+        }
+
+        if (Input.GetButtonUp("hammer"))
+         {
+            startTime = 0;
+         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isOnTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            isOnTrigger = false;
+        }
+    }
+}
