1e3cae599edeee12e0472b7191188e7b8e57ab9a
dyg006 <diego_yguay2@hotmail.com>
2020-02-01 16:09:46 +0100

    obbeying the orders of DavidQA

--- Assets/Scripts/Sickle/SickleMovement.cs
+++ Assets/Scripts/Sickle/SickleMovement.cs
@@ -29,7 +29,7 @@ public class SickleMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        sickleCollider = GetComponent<BoxCollider2D>();
+        sickleCollider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
