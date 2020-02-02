2136dd30c3711a0c409a3f755e9003ec246c2ab6
ferdelmo <ferdelmo1996@gmail.com>
2020-02-02 02:41:58 +0100

    Mejorando el movimienot de los enemigos

--- Assets/Scripts/AI/AIMeleeAttack.cs
+++ Assets/Scripts/AI/AIMeleeAttack.cs
@@ -59,4 +59,12 @@ public class AIMeleeAttack : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().Damage(damage);
+        }
+    }
+
 }
